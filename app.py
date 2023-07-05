0813975e6c0fcfb31a45d59671c15cd4ba29dc41
sonusz <zhongxingsi@gmail.com>
2023-07-05 10:38:14 -0700

    Init Support Azure

--- app.py
+++ app.py
@@ -10,13 +10,20 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 @app.route("/", methods=("GET", "POST"))
 def index():
     if request.method == "POST":
-        animal = request.form["animal"]
-        response = openai.Completion.create(
-            model="text-davinci-003",
-            prompt=generate_prompt(animal),
-            temperature=0.6,
-        )
-        return redirect(url_for("index", result=response.choices[0].text))
+        animal = request.form["animal"]        
+        resource_name = 'gpcs-nw-west-europe'
+        openai.api_base = f'https://{resource_name}.openai.azure.com/'
+        openai.api_type = 'azure'
+        openai.api_version = '2023-05-15'
+        deployment_id = 'gpcs-nw-west-europe-poc'
+
+        response = openai.ChatCompletion.create(
+                deployment_id=deployment_id,
+                stream=False,
+                messages=[{'role':'system', 'content': generate_prompt(animal)}],
+                temperature=0.6
+            )
+        return redirect(url_for("index", result=response['choices'][0]['message'].get('content', 'No response from OpenAI')))
 
     result = request.args.get("result")
     return render_template("index.html", result=result)
